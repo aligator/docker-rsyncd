f08f74ced0f6bcd91d06201f2da14df2171dfa20
aligator <aligator@suncraft-server.de>
2023-11-23 19:36:26 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.10
+FROM ubuntu:22.04
 MAINTAINER thomfab
 
 RUN apt-get update && \
--- run.sh
+++ run.sh
@@ -3,17 +3,24 @@ VOLUME=${VOLUME:-"volume"}
 ALLOW=${ALLOW:-192.168.0.0/16 172.16.0.0/12}
 OWNER=${OWNER:-65534}
 GROUP=${GROUP:-65534}
+GROUPNAME=${GROUPNAME=rsyncdgroup}
+USERNAME=${USERNAME=rsyncduser}
+PASSWORD=${PASSWORD}
 
 # create users matching ids passed if necessary
 if [[ ${GROUP} -ne 65534 && ${GROUP} -ge 1000 ]]; then
   if getent group ${GROUP} ; then groupdel ${GROUP}; fi
-  groupadd -g ${GROUP} rsyncdgroup
+  groupadd -g ${GROUP} ${GROUPNAME}
 fi
 
 if [[ ${OWNER} -ne 65534 && ${OWNER} -ge 1000 ]]; then
   if getent passwd ${OWNER} ; then userdel -f ${OWNER}; fi
-  useradd -l -u ${OWNER} -g ${GROUP} rsyncduser
+  useradd -l -u ${OWNER} -g ${GROUP} ${USERNAME}
 fi
+
+echo "$USERNAME:$PASSWORD" > /etc/rsyncd.secrets
+chgrp $USERNAME /etc/rsyncd.secrets
+chmod 0440 /etc/rsyncd.secrets
 
 [ -f /etc/rsyncd.conf ] || cat <<EOF > /etc/rsyncd.conf
 uid = ${OWNER}
@@ -27,6 +34,8 @@ log file = /dev/stdout
     read only = false
     path = /volume
     comment = ${VOLUME}
+    auth users = $USERNAME
+    secrets file = /etc/rsyncd.secrets
 EOF
 
 exec /usr/bin/rsync --no-detach --daemon --config /etc/rsyncd.conf "$@"
